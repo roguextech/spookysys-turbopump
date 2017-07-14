c41362a4835a071c8338977e7bc5256e17a392d9
Eivind Liland <spookysys@gmail.com>
2017-07-14 21:27:04 +0200

    Added pump_constant curves

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 import passes.empirical.lobanoff.impeller.head_rise
+import passes.empirical.lobanoff.impeller.head_constant
 
 # hello world
-
--- passes/empirical/lobanoff/impeller/head_rise.py
+++ passes/empirical/lobanoff/impeller/head_rise.py
@@ -2,38 +2,39 @@ print(__name__)
 from . import DATA as IMPELLER_DATA
 import scipy as sp
 import numpy as np
-from scipy.interpolate import interp1d
 from matplotlib import pyplot as plt
 
 DATA = IMPELLER_DATA["head_rise"]
 
 
 def _init():
-    for iter in DATA:
-        n_s, percent_head_rise = np.transpose(iter["data"]).tolist()
-        iter["fit"] = np.polyfit(n_s, percent_head_rise, 4)
+    for datum in DATA:
+        N_s, percent_head_rise = np.transpose(datum["data"]).tolist()
+        datum["fit"] = np.polyfit(N_s, percent_head_rise, 4)
 
 
 def _plot_data():
-    for data in DATA:
-        n_s, percent_head_rise = np.transpose(data["data"]).tolist()
-        fitted_n_s = np.arange(0, n_s[-1]+1000, 100)
-        fitted_vals = np.polyval(data["fit"], fitted_n_s)
-        label = str(data["vanes"]) + " vanes, " \
-            + str(data["discharge_angle"]) + " deg" \
-            + (", droop" if data["droop"] else "")
-        label_xy = (n_s[-1], percent_head_rise[-1])
-        plt.plot(fitted_n_s, fitted_vals, 'g--', 
-                 n_s, percent_head_rise, 'r',
+    for datum in DATA:
+        N_s, percent_head_rise = np.transpose(datum["data"]).tolist()
+        fitted_N_s = np.arange(0, N_s[-1]+1000, 100)
+        fitted_vals = np.polyval(datum["fit"], fitted_N_s)
+        label = str(datum["vanes"]) + " vanes, " \
+            + str(datum["discharge_angle"]) + " deg" \
+            + (", droop" if datum["droop"] else "")
+        label_xy = (N_s[-1], percent_head_rise[-1])
+        plt.plot(fitted_N_s, fitted_vals, 'g--',
+                 N_s, percent_head_rise, 'r',
                  label=label)
         plt.annotate(label, xy=label_xy)
+    plt.gca().set_xticks(np.arange(0, 3601, 400))
+    plt.gca().set_yticks(np.arange(0, 65, 5))
     plt.grid()
     plt.show()
 
 
-def get(vanes, n_s):
+def get(vanes, N_s):
     tmp = next(x for x in DATA if x["vanes"] == vanes)
-    return np.polyval(tmp["fit"], n_s)
+    return np.polyval(tmp["fit"], N_s)
 
 
 _init()
